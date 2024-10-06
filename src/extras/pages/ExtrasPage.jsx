581e2fc685d644a2d28abf075adb4d3d90e50ba0
MaxiDesarrollador <chamarrodesarrollador@gmail.com>
2024-10-06 09:38:22 -0300

    Parte Grafica terminada

--- src/extras/pages/ExtrasPage.jsx
+++ src/extras/pages/ExtrasPage.jsx
@@ -1,17 +1,36 @@
+import { IconButton } from '@mui/material';
 import { ExtrasLayout } from '../layout/ExtrasLayout'
 import { NoteView } from '../views/NoteView';
 import { NothingSelectedView } from '../views/NothingSelectedView';
+import { AddOutlined } from '@mui/icons-material';
 
-const drowerWidth =  240;
+const drowerWidth = 240;
 
 export const ExtrasPage = () => {
   return (
     <>
       <ExtrasLayout>
         {/* <Typography variant='h1'>ExtrasPage</Typography> */}
-        {/* <NothingSelectedView/> */}
-        <NoteView/>
-      </ExtrasLayout>
+        <NothingSelectedView />
+        {/* <NoteView/> */}
+
+        <IconButton
+          size='large'
+          sx={{
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+            position: 'fixed',
+            right: 50,
+            bottom: 50
+          }}
+        >
+          <AddOutlined sx={{ fontSize: 30 }} />
+
+        </IconButton>
+
+
+      </ExtrasLayout >
     </>
   )
 }
